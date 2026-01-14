65377063b5efa0a5d8d5113ead770de703801876
mDeepaksai <mdeepaksai806@gmail.com>
2026-01-14 19:52:38 +0530

    backend

---
+++ index.js
@@ -0,0 +1,37 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8Vm0bP5bbMtA9YEKT-p1gh7WyiTkZl5M",
+  authDomain: "login-4f2a9.firebaseapp.com",
+  projectId: "login-4f2a9",
+  storageBucket: "login-4f2a9.firebasestorage.app",
+  messagingSenderId: "827317854395",
+  appId: "1:827317854395:web:f81fea3190069357d6499e"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
+
+  if (!email || !password) {
+    alert("Email and password required");
+    return;
+  }
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      alert("Login successful");
+      console.log(userCredential.user.uid);
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+});
---
+++ newuser.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8Vm0bP5bbMtA9YEKT-p1gh7WyiTkZl5M",
+  authDomain: "login-4f2a9.firebaseapp.com",
+  projectId: "login-4f2a9",
+  storageBucket: "login-4f2a9.firebasestorage.app",
+  messagingSenderId: "827317854395",
+  appId: "1:827317854395:web:f81fea3190069357d6499e"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
+
+  if (!email || !password) {
+    alert("Email and password required");
+    return;
+  }
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert("User registered successfully");
+    window.location.href = "login.html";
+  } catch (err) {
+    alert(err.message);
+  }
+});
